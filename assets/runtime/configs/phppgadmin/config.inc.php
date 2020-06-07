ec008831a57f832b0457d768318221eb60e81153
Kacper Jurak <kacper@jurak.pl>
2020-06-07 21:30:43 +0200

    Update config.inc.php

--- assets/runtime/configs/phppgadmin/config.inc.php
+++ assets/runtime/configs/phppgadmin/config.inc.php
@@ -32,8 +32,8 @@ $conf['servers'][0]['defaultdb'] = 'template1';
 
 // Specify the path to the database dump utilities for this server.
 // You can set these to '' if no dumper is available.
-$conf['servers'][0]['pg_dump_path'] = '/usr/bin/pg_dump';
-$conf['servers'][0]['pg_dumpall_path'] = '/usr/bin/pg_dumpall';
+$conf['servers'][0]['pg_dump_path'] = '/usr/local/bin/pg_dump';
+$conf['servers'][0]['pg_dumpall_path'] = '/usr/local/bin/pg_dumpall';
 
 // Example for a second server (PostgreSQL for Windows)
 //$conf['servers'][1]['desc'] = 'Test Server';
